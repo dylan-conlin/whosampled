4bbf824840565c262308eed3b22a425943c02b27
Dylan Conlin <dylan.conlin@gmail.com>
2016-06-11 11:33:37 -0700

    Added tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,34 @@
 import {expect} from 'chai';
-import {sum} from '../';
+import {WhoSampled} from '../';
 const {describe, it} = global;
+const whosamp = new WhoSampled();
 
-describe('sum', () => {
-  it('should add two numbers correctly', async () => {
-    const result = await sum(10, 20);
-    expect(result).to.be.equal(30);
+describe('WhoSampled', () => {
+  it('should be initialized', () => {
+    expect(whosamp.init()).to.equal("initialized");
   });
+
+  describe('connections', () => {
+    it('should return if no query is provided', () => {
+      whosamp.connections({q: null}).catch((result) => {
+        expect(result).to.equal("Please supply a query")
+      })
+      whosamp.connections().catch((result) => {
+        expect(result).to.equal("Please supply a query")
+      })
+    })
+
+    it('returns a single sample', async () => {
+      const sampleUrl = "http://www.whosampled.com/sample/24/Kanye-West-Jamie-Foxx-Gold-Digger-Ray-Charles-I-Got-a-Woman/";
+      const result = await whosamp.sample(sampleUrl)
+    })
+
+    it.only('returns all samples', async () => {
+      const result = await whosamp.samples({q: 'maceo plex', limit: 1});
+    })
+
+
+  })
 });
+
+
